5b357e0a42471c43ef06f10fc51459779130554b
MdShamimIslam <shamim401897@gmail.com>
2023-12-24 00:13:50 +0600

    filter api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j5nrexn.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -17,77 +16,91 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
     // collection
-    const foodCollection = client.db('foodBuzz').collection('foods');
-    const requestFoodCollection = client.db('foodBuzz').collection('requestFoods');
+    const foodCollection = client.db("foodBuzz").collection("foods");
+    const requestFoodCollection = client
+      .db("foodBuzz")
+      .collection("requestFoods");
 
-   // FOOD RELATED API
-    app.get('/limitFoods', async(req,res)=>{
-        const result = await foodCollection.find().limit(6).sort({ "quantity": -1 }).toArray();
-        res.send(result);
-    })
+    // FOOD RELATED API
+    app.get("/limitFoods", async (req, res) => {
+      const result = await foodCollection
+        .find()
+        .limit(6)
+        .sort({ quantity: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/foods', async(req,res)=>{
-        const result = await foodCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/foods", async (req, res) => {
+      let query = {};
+      const foodName = req.query.food_name;
+      if (foodName) {
+        query = { food_name: foodName };
+      }
+      const result = await foodCollection
+        .find(query)
+        .sort({ expired_date: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/foods/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = { _id : new ObjectId(id)};
-        const result = await foodCollection.findOne(query);
-        res.send(result);
-    })
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/createFood', async(req,res)=>{
+    app.post("/createFood", async (req, res) => {
       const food = req.body;
       const result = await foodCollection.insertOne(food);
       res.send(result);
-    })
+    });
 
-    app.get('/createFood', async(req,res)=>{
+    app.get("/createFood", async (req, res) => {
       const email = req.query.email;
-      const query = { donator_email : email };
+      const query = { donator_email: email };
       const result = await foodCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     // REQUEST FOOD RELATED API
-    app.get('/requestFood', async(req,res)=>{
+    app.get("/requestFood", async (req, res) => {
       const email = req.query.email;
-      const query = { requester_email : email}
+      const query = { requester_email: email };
       const result = await requestFoodCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/requestFood', async(req,res)=>{
+    app.post("/requestFood", async (req, res) => {
       const food = req.body;
       const result = await requestFoodCollection.insertOne(food);
       res.send(result);
-    })
+    });
 
-    app.delete('/requestFood/:id', async(req,res)=>{
+    app.delete("/requestFood/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id : new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await requestFoodCollection.deleteOne(query);
       res.send(result);
-    })
-
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Community food server is running");
